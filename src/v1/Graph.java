fbd6188c0d0b058f7d07dcc6337461478db72d69
Legendary-Overlord <47962278+Legendary-Overlord@users.noreply.github.com>
2019-11-08 00:18:06 -0500

    Added Dijkstra on V1 Graph

--- src/v1/Graph.java
+++ src/v1/Graph.java
@@ -1,10 +1,12 @@
 package v1;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
@@ -46,7 +48,7 @@ public class Graph<E> {
 	    return adjVertices.get(new Vertex<E>(e));
 	}
 	public Set<Vertex<E>> depthFirstSearch(Graph<E> graph, Vertex<E> root) {
-		resetVertexCalculations(graph,root);
+		resetVertices();
 	    Set<Vertex<E>> visited = new LinkedHashSet<Vertex<E>>();
 	    Stack<Vertex<E>> stack = new Stack<Vertex<E>>();
 	    stack.push(root);
@@ -62,7 +64,7 @@ public class Graph<E> {
 	    return visited;
 	}
 	public Set<Vertex<E>> breadthFirstSearch(Graph<E> graph, Vertex<E> root) {
-		resetVertexCalculations(graph,root);
+		resetVertices();
 	    Set<Vertex<E>> visited = new LinkedHashSet<Vertex<E>>();
 	    Queue<Vertex<E>> queue = new LinkedList<Vertex<E>>();
 	    queue.add(root);
@@ -96,24 +98,63 @@ public class Graph<E> {
 			lhs.add(a);
 		}
 	}
-	private void resetVertexCalculations(Graph<E> graph, Vertex<E> root) {
-		root.dist=0;
-		root.pre=null;
-		Set<Vertex<E>> visited = new LinkedHashSet<Vertex<E>>();
-	    Queue<Vertex<E>> queue = new LinkedList<Vertex<E>>();
-	    queue.add(root);
-	    visited.add(root);
-	    while (!queue.isEmpty()) {
-	    	Vertex<E> Vertex = queue.poll();
-	        for (Vertex<E> v : graph.getAdjVertices(Vertex.obj)) {
-	            if (!visited.contains(v)) {
-	            	v.dist=99999999;
-	            	v.pre=null;
-	                visited.add(v);
-	                queue.add(v);
-	            }
-	        }
-	    }
+//	private void resetVertexCalculations(Graph<E> graph, Vertex<E> root) {
+//		root.dist=0;
+//		root.pre=null;
+//		Set<Vertex<E>> visited = new LinkedHashSet<Vertex<E>>();
+//	    Queue<Vertex<E>> queue = new LinkedList<Vertex<E>>();
+//	    queue.add(root);
+//	    visited.add(root);
+//	    while (!queue.isEmpty()) {
+//	    	Vertex<E> Vertex = queue.poll();
+//	        for (Vertex<E> v : graph.getAdjVertices(Vertex.obj)) {
+//	            if (!visited.contains(v)) {
+//	            	v.dist=99999999;
+//	            	v.pre=null;
+//	                visited.add(v);
+//	                queue.add(v);
+//	            }
+//	        }
+//	    }
+//	}
+	private void resetVertices() {
+		for(Map.Entry<Vertex<E>, List<Vertex<E>>> v : adjVertices.entrySet()) {
+			Vertex<E> vertex = v.getKey();
+			vertex.dist=Vertex.INFINITY;
+			vertex.pre=null;
+		}
+	}
+	public void dijkstra(Graph<E> graph, Vertex<E> a, Vertex<E> b) {
+		a.dist=0;
+		PriorityQueue<Vertex<E>> Q = new PriorityQueue<>(new PQComparator());
+		for(Map.Entry<Vertex<E>, List<Vertex<E>>> v: adjVertices.entrySet()) {
+			Vertex<E> temp=null;
+			if((temp=v.getKey())!=a) {
+				temp.dist=Vertex.INFINITY;
+			}
+			temp.pre=null;
+			Q.add(temp);
+		}
+		while(!Q.isEmpty()) {
+			Vertex<E> u = Q.poll();
+			 for (Vertex<E> v : graph.getAdjVertices(u.obj)) {
+		           double alt = v.dist; //+weight(u,v)
+		           if(alt<v.dist) {
+		        	   v.dist=alt;
+		        	   v.pre=u;
+		        	   Q.remove(v);
+		        	   Q.add(v);
+		           }
+		        }
+		}
 	}
 
+}
+class PQComparator implements Comparator<Vertex>{
+	@Override
+	public int compare(Vertex o1, Vertex o2) {
+		Double a1=o1.dist;
+		Double a2 = o2.dist;
+		return a2.compareTo(a1);
+	}
 }
--- src/v1/Vertex.java
+++ src/v1/Vertex.java
@@ -1,13 +1,16 @@
 package v1;
 
 public class Vertex<E> {
+	
+	static final double INFINITY=99999999999999999999999999999.9;
+	
 	E obj;
 	Vertex<E> pre;
 	double dist;
 	public Vertex(E obj) {
 		this.obj = obj;
 		this.pre = null;
-		this.dist = 99999999999999999999999999999.9;
+		this.dist = INFINITY;
 	}
 	
 	
