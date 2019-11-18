e694a95afaac5ef1600ef9d58f3ded1203800220
Legendary-Overlord <47962278+Legendary-Overlord@users.noreply.github.com>
2019-11-17 23:01:22 -0500

    added kruskal to v1

--- src/v1/Graph.java
+++ src/v1/Graph.java
@@ -224,6 +224,68 @@ public class Graph<E> {
 		        }
 		}
 	}
+	public void makeSet(int [] parent){
+        //Make set- creating a new element with a parent pointer to itself.
+        for (int i = 0; i <gSize ; i++) {
+            parent[i] = i;
+        }
+    }
+	public int find(int [] parent, int vertex){
+        //chain of parent pointers from x upwards through the tree
+        // until an element is reached whose parent is itself
+        if(parent[vertex]!=vertex)
+            return find(parent, parent[vertex]);;
+        return vertex;
+    }
+	public void union(int [] parent, int x, int y){
+        int x_set_parent = find(parent, x);
+        int y_set_parent = find(parent, y);
+        //make x as parent of y
+        parent[y_set_parent] = x_set_parent;
+    }
+	public void kruskal(){
+        PriorityQueue<Edge<E>> pq = new PriorityQueue<>(adjVertices.size(), Comparator.comparingDouble(o -> o.weight));
+
+        //add all the edges to priority queue, //sort the edges on weights
+        for(Edge<E> e: edgeWeight)
+        	pq.add(e);
+        //create a parent []
+        int [] parent = new int[gSize];
+
+        //makeset
+        makeSet(parent);
+
+        ArrayList<Edge<E>> mst = new ArrayList<>();
+
+        //process vertices - 1 edges
+        int index = 0;
+        while(index<gSize-1){
+            Edge<E> edge = pq.remove();
+            //check if adding this edge creates a cycle
+            int x_set = find(parent, vertexNum.get(edge.a));
+            int y_set = find(parent, vertexNum.get(edge.b));
+
+            if(x_set==y_set){
+                //ignore, will create cycle
+            }else {
+                //add it to our final result
+                mst.add(edge);
+                index++;
+                union(parent,x_set,y_set);
+            }
+        }
+        //print MST
+        System.out.println("Minimum Spanning Tree: ");
+        printMST(mst);
+    }
+	   public void printMST(ArrayList<Edge<E>> edgeList){
+           for (int i = 0; i <edgeList.size() ; i++) {
+               Edge<E> edge = edgeList.get(i);
+               System.out.println("Edge-" + i + " source: " + edge.a +
+                       " destination: " + edge.a +
+                       " weight: " + edge.weight);
+           }
+	   }
 }
 @SuppressWarnings("rawtypes")
 class PQComparator implements Comparator<Vertex>{
