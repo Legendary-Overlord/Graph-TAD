08255ace24fa2b65d1168ddf3769e8266105f223
Matein092 <47835701+Matein092@users.noreply.github.com>
2019-11-22 12:40:09 -0500

    Added some news algorithms and new classes

--- src/v2/VertexAM.java
+++ src/v2/VertexAM.java
@@ -1,17 +1,74 @@
 package v2;
 
-public class VertexAM<E> {
+public class VertexAM<T> implements Comparable<VertexAM<T>> {
+	public final static int WHITE = 0;
+	public final static int GRAY = 1;
+	public final static int BLACK = 2;
+	private T value;
+	private int d;
+	private int f;
+	private int color;
+	private VertexAM<T> pred;
 	
-
-	static final double INFINITY=99999999999999999999999999999.9;
-	E obj;
-	VertexAM<E> pre;
-	double dist;
-	
-	public VertexAM(E obj) {
-		this.obj = obj;
-		this.pre = null;
-		this.dist = INFINITY;
+	public VertexAM(T type) {
+		value = type;
 	}
+
+	public T getValue() {
+		return value;
+	}
+
+	public void setValue(T value) {
+		this.value = value;
+	}
+
+	public int getD() {
+		return d;
+	}
+
+	public void setD(int d) {
+		this.d = d;
+	}
+
+	public int getF() {
+		return f;
+	}
+
+	public void setF(int f) {
+		this.f = f;
+	}
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		this.color = color;
+	}
+
+	public VertexAM<T> getPred() {
+		return pred;
+	}
+
+	public void setPred(VertexAM<T> pred) {
+		this.pred = pred;
+	}
+
+	@Override
+	public String toString() {
+		return ""+ value;
+	}
+
+	@Override
+	public int compareTo(VertexAM<T> o) {
+		if(d < o.getD()) {
+			return -1;
+		}else if(d > o.getD()) {
+			return 1;
+		}else
+			return 0;
+	}
+
+	
 }
 		
